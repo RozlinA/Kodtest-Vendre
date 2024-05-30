400a52880969083f66969a0c6cd363d5cd6cd140
Rözlin Akkurt <rozlin.akkurt@hotmail.com>
2024-05-30 21:26:32 +0200

    Fetched API

---
+++ src/components/Employees.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/* __placeholder__ */
+import type { IApiResponse } from '@/models/IApiResponse';
+import type { IEmployee } from '@/models/IEmployee';
+import { onMounted, ref } from 'vue';
+
+
+const employees = ref<IEmployee[]>([]);
+
+  onMounted(async () => {
+    const response = await fetch("https://reqres.in/api/users");
+    const data: IApiResponse = await response.json();
+    employees.value = data.data;
+    console.log(data.data)
+  });
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
